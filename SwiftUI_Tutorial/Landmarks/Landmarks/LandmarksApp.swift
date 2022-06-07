9f995e3f2577996c0d6990489e872a4028b976b5
Dorii0513 <bobodorii0513@gmail.com>
2022-06-07 22:24:51 +0900

    0607

---
+++ SwiftUI_Tutorial/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김예림 on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
