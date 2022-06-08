ce4471edb5e5372b6483a43d24540b207119517b
Dorii0513 <bobodorii0513@gmail.com>
2022-06-08 23:40:18 +0900

    0608

--- SwiftUI_Tutorial/Landmarks/Landmarks/ContentView.swift
+++ SwiftUI_Tutorial/Landmarks/Landmarks/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
