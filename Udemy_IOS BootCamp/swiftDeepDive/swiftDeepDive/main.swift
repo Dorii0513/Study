bd090da9db50a59802b5c957abee263826800aea
Dorii0513 <bobodorii0513@gmail.com>
2022-06-21 01:31:50 +0900

    0621

---
+++ Udemy_IOS BootCamp/swiftDeepDive/swiftDeepDive/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  swiftDeepDive
+//
+//  Created by 김예림 on 2022/06/21.
+//
+
+import Foundation
+
+func greeting() {
+    print("hello")
+}
+
+for _ in 1...3 {
+    greeting()
+}
