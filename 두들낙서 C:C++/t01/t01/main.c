628958c0ca2c1ee52c1f051c0a95f056995c641b
Dorii0513 <bobodorii0513@gmail.com>
2022-07-18 23:19:29 +0900

    0718

---
+++ 두들낙서 C:C++/t01/t01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  t01
+//
+//  Created by 김예림 on 2022/07/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
